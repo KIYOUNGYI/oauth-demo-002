999e8dfb5e1986be6c875981c3de1db90c3fdb77
kiyoung <yky2798@gmail.com>
2018-05-26 16:42:26 +0900

    oauth 서버, api 서버 분리

--- oauth-server-001/src/main/java/com/kiyoung/security/oauthserver001/OauthServer001Application.java
+++ oauth-server-001/src/main/java/com/kiyoung/security/oauthserver001/OauthServer001Application.java
@@ -1,12 +1,23 @@
 package com.kiyoung.security.oauthserver001;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
+@EnableAuthorizationServer
 @SpringBootApplication
 public class OauthServer001Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthServer001Application.class, args);
+	}
+	@Bean
+	public TokenStore jdbcTokenStore(DataSource dataSource) {
+		return new JdbcTokenStore(dataSource);
 	}
 }
