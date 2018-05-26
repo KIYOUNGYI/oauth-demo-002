5d44bfcad1b4dde4cfb340ddf6f1ec30dd8555f3
kiyoung <yky2798@gmail.com>
2018-05-26 15:56:44 +0900

    2nd commit

---
+++ oauth-api-server-001/src/main/java/com/kiyoung/security/oauthapiserver001/OauthApiServer001Application.java
@@ -0,0 +1,12 @@
+package com.kiyoung.security.oauthapiserver001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthApiServer001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApiServer001Application.class, args);
+	}
+}
---
+++ oauth-server-001/src/main/java/com/kiyoung/security/oauthserver001/OauthServer001Application.java
@@ -0,0 +1,12 @@
+package com.kiyoung.security.oauthserver001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServer001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthServer001Application.class, args);
+	}
+}
